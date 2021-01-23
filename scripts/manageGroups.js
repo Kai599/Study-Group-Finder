4807cb072b2ba0e1d28e48bd06a384cdeb0f63b4
Maggus407 <kauschmann1855@gmail.com>
2021-01-23 16:12:03 +0100

    search funktioniert soweit

--- scripts/manageGroups.js
+++ scripts/manageGroups.js
@@ -4,26 +4,37 @@ onload = (e) => {
   usereingabe = document.getElementById('search');
 }
 
+
 //groups
-function group(name, fach){
+function group(name, fach,beschreibung,anzahl){
   this.name = name;
   this.fach = fach;
+  this.beschreibung = beschreibung;
+  this.anzahl = anzahl;
 }
 
-var group1 = new group("Die Informierer", "Informatik");
-var group2 = new group("G", "Wirtschaftsinformatik");
-var group3 = new group("Meisdas", "Informatik");
+var group1 = new group("Die Informierer", "Informatik",
+"Hi, wir sind die Informierer und suchen noch hoffnungsvoll nach neuen Mitgliedern!",
+"3/5"
+);
+var group2 = new group("G", "Wirtschaftsinformatik",
+ "Wenn es bei dir auch nicht fuer ein richtige Informatik Studium gereicht hat, dann komm in unsere Hoffnungslose Gruppe",
+"4/12"
+);
+var group3 = new group("Progers", "Informatik");
 var group4 = new group("Biwis", "BWL");
 var group5 = new group("Speakers", "Spanisch");
+var group6 = new group("Infos", "Informatik");
+var group7 = new group("Anas", "Mathe");
 
-var groups = [group1,group2,group3,group4,group5];
+var groups = [group1,group2,group3,group4,group5,group6,group7];
 
 
 function add(element){
   groups.push(element);
 }
-function create(name,fach){
-  var group = new group(name,fach);
+function create(name,fach,beschreibung){
+  var group = new group(name,fach,beschreibung);
   add(group);
 }
 
@@ -32,38 +43,70 @@ function searchGroup(){
   const input = usereingabe.value;
   let h = document.getElementById('search').value;
   if(h.length == 0){
-    console.log('1');
     deleteLI();
+    schließeFenster();
   }
-
-  if(groups.filter(x => x.name.toUpperCase() === input.toUpperCase()) || groups.filter(x => x.fach.toUpperCase() === input.toUpperCase())){
+  if(groups.filter(x => x.name.toUpperCase() === input.toUpperCase())){
     const name = groups.filter(x => x.name.toUpperCase() === input.toUpperCase());
-    const fach = groups.filter(x => x.fach.toUpperCase() === input.toUpperCase());
-    if(name.length > 0 || fach.length > 0){
+    if(name.length > 0){
       addListeUI(name);
+    }
+  }
+  if(groups.filter(x => x.fach.toUpperCase() === input.toUpperCase())){
+    const fach = groups.filter(x => x.fach.toUpperCase() === input.toUpperCase());
+    if(fach.length > 0){
       addListeUI(fach);
     }
-  }else{
-    console.log("Keine Gruppe/Fach vorhanden");
   }
 }
 
 //Fügt automatisch die Gruppen im UI hinzu
 function addListeUI(element){
-  console.log(element);
-  let group = element;
 
-  for (var g of group) {
-    listNode = document.getElementById('list');
-    liNode = document.createElement("LI");
-    txtNode = document.createTextNode(g.name + " " + g.fach);
-    liNode.appendChild(txtNode);
-    listNode.appendChild(liNode);
-  }
+const tabelle = document.getElementById('tabelle');
+let stuff = '';
+for (var s of element) {
+  stuff += `<tr id="change"><td onclick="getInformation(this)" id="s">${s.name}</td><td>${s.fach}</td></tr>`;
+}
+
+tabelle.innerHTML = stuff;
 }
 
 //Entfernt die Liste im UI, wenn in der Suchleiste nichts steht
 function deleteLI(){
-var g = document.getElementById('list');
+var g = document.getElementById('tabelle');
 g.innerHTML = '';
+}
+
+//Zeigt die infos der Gruppe, wenn man auf sie drückt
+let tabe = document.getElementById('tabelle');
+let dialog = document.getElementById('infos');
+let abbruch = document.getElementById('abbrechen');
+tabe.addEventListener('click',getInformation);
+abbruch.addEventListener('click',schließeFenster);
+
+
+function schließeFenster() {
+  dialog.style.display = "none";
+}
+
+function getInformation(element){
+  let info = document.getElementById('groupinfo');
+  let anzahl = document.getElementById('anzahl');
+let name = element.innerText;
+for (var v of groups) {
+  if(name === v.name){
+    anzahl.innerHTML = v.anzahl;
+    info.innerHTML = v.beschreibung;
+    dialog.style.display = "block";
+
+  }
+ }
+}
+
+
+function beitreten(){
+  let buttonYes = document.getElementById('beitreten');
+  buttonYes.addEventListener('click',alert("Sie sind erfolgreich beigetreten"));
+  buttonYes.disabled = true;
 }
